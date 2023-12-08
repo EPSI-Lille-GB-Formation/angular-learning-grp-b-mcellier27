024fc5795d0ef68c9119a1c8f94dd68ac38e4ccd
mcellier27 <mcellier27.pro@gmail.com>
2023-12-08 17:14:40 +0100

    main

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from './mock-todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template:`
-    <h1> Hello World </h1>
+    <h1> Liste des choses à faire </h1>
+    <ul>
+      <li> {{selectTodo(0).title}}</li>
+      <li> {{selectTodo(1).title}}</li>
+      <li> {{selectTodo(2).title}}</li>
+      <li> {{selectTodo(3).title}}</li>
+      <li> {{selectTodo(4).title}}</li>
+      <li> {{selectTodo(5).title}}</li>
+      <li> {{selectTodo(6).title}}</li>
+      <li> {{selectTodo(7).title}}</li>
+      <li> {{selectTodo(8).title}}</li>
+      <li> {{selectTodo(9).title}}</li>
+      <li> {{selectTodo(10).title}}</li>
   `
+  ,
+  styles: []
 })
 export class AppComponent {
-  title = 'todolist';
+
+  todoList = TODOS
+
+  constructor(){
+    console.table(this.todoList)
+  }
+  
+  selectTodo(id : number){
+    return this.todoList[id];
+  }
+  
 }
